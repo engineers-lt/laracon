ac7d7841ce96552dd2141ab36d33c89e5eb82759
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-02-07 11:23:52 +0900

    add Template

---
+++ tests/Template.php
@@ -0,0 +1,15 @@
+<?php
+
+class /**クラス名 */ extends PHPUnit\Framework\TestCase
+{
+    public function /**ファンクション名 */()
+    {
+        /** クラスを生成します
+         * $ws1 = new Laracon\WS1();
+        */
+
+        /** クラスのメソッドを $this->assertEqualsでテストしましょう
+         * $this->assertEquals("HelloWorld", $ws1->HelloWorld());
+         */
+    }
+}
